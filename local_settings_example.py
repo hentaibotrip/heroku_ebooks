d02dba6e3d02debccc55961ecb0c06e89cc2e266
hentaibotrip <31582587+hentaibotrip@users.noreply.github.com>
2017-09-02 22:26:28 -0700

    Update local_settings_example.py

--- local_settings_example.py
+++ local_settings_example.py
@@ -1,17 +1,17 @@
 '''
-Local Settings for a heroku_ebooks account. #fill in the name of the account you're tweeting from here.
+Hentaibotrip
 '''
 
 #configuration
-MY_CONSUMER_KEY = 'Your Twitter API Consumer Key'
-MY_CONSUMER_SECRET = 'Your Consumer Secret Key'
-MY_ACCESS_TOKEN_KEY = 'Your Twitter API Access Token Key'
-MY_ACCESS_TOKEN_SECRET = 'Your Access Token Secret'
+MY_CONSUMER_KEY = 'fip26bAnp2a9KBEJ2OYdSHz0R'
+MY_CONSUMER_SECRE= '5utorgXKqdqWMYRreiliJ1Lhppz3CMVMmfikDI4zRWxSutktYx'
+MY_ACCESS_TOKEN_KEY = '	894296331984003072-K8Eaif8L6QfcVmWsY780JiUkHkD5i4L'
+MY_ACCESS_TOKEN_SECRET = '	oc6gXxbuVrgKR9ZMdUDRm0LGyBkZcMtZ9uSpzYLFgh0Ry'
 
-SOURCE_ACCOUNTS = [""] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-ODDS = 8 #How often do you want this to run? 1/8 times?
-ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
-DEBUG = True #Set this to False to start Tweeting live
-STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
-TEST_SOURCE = ".txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
-TWEET_ACCOUNT = "" #The name of the account you're tweeting to.
+SOURCE_ACCOUNTS = ["@hentaibongrip"]
+ODDS = 8
+ORDER = 2
+DEBUG = True
+STATIC_TEST = False
+TEST_SOURCE = ".txt"
+TWEET_ACCOUNT = "" 
